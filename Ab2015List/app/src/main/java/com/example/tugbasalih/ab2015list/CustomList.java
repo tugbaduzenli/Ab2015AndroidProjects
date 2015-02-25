7e7c32457c7271395b786e5339a066d502284a56
tugbaduzenli <tugbadznl@gmail.com>
2015-02-25 15:43:09 +0200

    Listview kullanımı

---
+++ Ab2015List/app/src/main/java/com/example/tugbasalih/ab2015list/CustomList.java
@@ -0,0 +1,59 @@
+package com.example.tugbasalih.ab2015list;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class CustomList extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list);
+
+        final String[] list = getResources().getStringArray(R.array.strList);
+
+        final ListView lvList = (ListView)findViewById(R.id.lvList);
+        CustomAdapter adapter =  new CustomAdapter(this,R.layout.custom_row, list);
+        lvList.setAdapter(adapter);
+
+        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCity = list[position];
+                Toast.makeText(CustomList.this,selectedCity,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_custom_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
